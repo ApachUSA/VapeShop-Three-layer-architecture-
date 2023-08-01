1fa9de1858c22676a4c9bd927d32700f8c45a60d
ApachUA <4971779@gmail.com>
2023-08-01 18:45:53 +0300

    Added DbContext to services

--- VapeShop.Web/Program.cs
+++ VapeShop.Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using VapeShop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<VapeShopDbContext>(options =>
+						options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
