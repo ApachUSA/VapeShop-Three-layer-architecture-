71fa1652b4b706dfc9889d65f265f4486d2d621d
ApachUA <4971779@gmail.com>
2023-08-02 13:47:23 +0300

    Created IBaseRespository

---
+++ VapeShop.Infrastructure/Interfaces/IBaseRespository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VapeShop.Infrastructure.Interfaces
+{
+	public interface IBaseRespository<T> where T : class
+	{
+		Task Create(T entity);
+
+		Task Delete(T entity);
+
+		IQueryable<T> Get();
+
+		Task<T> Update(T entity);
+	}
+}
