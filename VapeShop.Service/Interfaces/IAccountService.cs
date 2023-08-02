151756141965f7314124c73b4dc02524fa9e3acc
ApachUA <4971779@gmail.com>
2023-08-02 16:16:35 +0300

    Created interfaces for user services

---
+++ VapeShop.Service/Interfaces/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Response;
+using VapeShop.Domain.ViewModels;
+
+namespace VapeShop.Service.Interfaces
+{
+	public interface IAccountService
+	{
+		Task<BaseResponse<ClaimsIdentity>> Register(RegisterVM model);
+
+		Task<BaseResponse<ClaimsIdentity>> Login(LoginVM model);
+	}
+}
---
+++ VapeShop.Service/Interfaces/IProfileService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Entity.Client;
+using VapeShop.Domain.Response;
+
+namespace VapeShop.Service.Interfaces
+{
+	public interface IProfileService
+	{
+		Task<BaseResponse<User>> GetProfile(int id);
+
+		Task<BaseResponse<User>> Update(int id);
+	}
+}
