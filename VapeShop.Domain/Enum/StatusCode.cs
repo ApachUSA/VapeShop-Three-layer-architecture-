896cc33417ccf78464e3aaea3dcd786287f77b64
ApachUA <4971779@gmail.com>
2023-08-02 15:01:57 +0300

    Created Status Code enum for Response

---
+++ VapeShop.Domain/Enum/StatusCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VapeShop.Domain.Enum
+{
+	public enum StatusCode
+	{
+		UserNotFound = 0,
+		UserAlreadyExists = 1,
+
+		OrderNotFound = 20,
+
+		Succes = 200,
+		InternalServerError = 500
+	}
+}
