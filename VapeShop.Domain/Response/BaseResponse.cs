9770c66644e8924aa95c643e54dc58dd0733bf4c
ApachUA <4971779@gmail.com>
2023-08-02 15:01:27 +0300

    Implementated IBaseResponse

---
+++ VapeShop.Domain/Response/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Enum;
+
+namespace VapeShop.Domain.Response
+{
+	public class BaseResponse<T> : IBaseResponse<T>
+	{
+		public string Descrition { get; set; }
+		public StatusCode StatusCode { get; set; }
+		public T Value { get; set; }
+	}
+}
