44ff3effbb1785a3726745289e308af77f57d2c2
ApachUA <4971779@gmail.com>
2023-08-02 15:00:47 +0300

    Created Response interface

---
+++ VapeShop.Domain/Response/IBaseResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Enum;
+
+namespace VapeShop.Domain.Response
+{
+	public interface IBaseResponse<T>
+	{
+		string Descrition { get; set; }
+
+		StatusCode StatusCode { get; set; }
+
+		T Value { get; set; }
+	}
+}
