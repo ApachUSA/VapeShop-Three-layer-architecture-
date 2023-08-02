5f9cceb1975315c4f767548cab2563ef4733278f
ApachUA <4971779@gmail.com>
2023-08-02 16:15:34 +0300

    Created User ViewModels

---
+++ VapeShop.Domain/ViewModels/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Entity.Client;
+
+namespace VapeShop.Domain.ViewModels
+{
+	public class UserViewModel
+	{
+		public int UserID { get; set; }
+
+		public required string Surname { get; set; }
+
+		public required string Name { get; set; }
+
+		public string? Phone { get; set; }
+
+		public required string Email { get; set; }
+
+		public int? CommunicationMethodID { get; set; }
+
+		public CommunicationMethod? CommunicationMethod { get; set; }
+
+		public int? DeliveryAddressID { get; set; }
+
+		public DeliveryAddress? DeliveryAddress { get; set; }
+	}
+}
