ee23a4e10c7544b7d471e36ed4e0c789d0f01d91
ApachUA <4971779@gmail.com>
2023-08-02 13:19:55 +0300

    Added dbcontext for tests

--- VapeShop.Web/Controllers/HomeController.cs
+++ VapeShop.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using VapeShop.Infrastructure;
 using VapeShop.Web.Models;
 
 namespace VapeShop.Web.Controllers
@@ -7,10 +8,12 @@ namespace VapeShop.Web.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly VapeShopDbContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, VapeShopDbContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
 
 		public IActionResult Index()
