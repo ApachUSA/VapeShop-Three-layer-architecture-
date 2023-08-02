797243d35fde09186ae5ee9921f0e4d23f94c25c
ApachUA <4971779@gmail.com>
2023-08-02 18:50:27 +0300

    Created interfaces for liquid_param services

---
+++ VapeShop.Service/Interfaces/ILiquidParamService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Entity.Product;
+using VapeShop.Domain.Response;
+
+namespace VapeShop.Service.Interfaces
+{
+	public interface ILiquidParamService
+	{
+		Task<BaseResponse<Liquid_param>> Get(int id);
+
+		Task<BaseResponse<IEnumerable<Liquid_param>>> GetAll();
+
+		Task<BaseResponse<Liquid_param>> Create(Liquid_param model);
+
+		Task<BaseResponse<Liquid_param>> Update(Liquid_param model);
+
+		Task<BaseResponse<bool>> Delete(Liquid_param model);
+	}
+}
