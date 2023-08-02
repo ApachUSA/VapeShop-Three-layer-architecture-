432d73024484cf3826b618fef4b5a2c58fb4644d
ApachUA <4971779@gmail.com>
2023-08-02 18:26:00 +0300

    fix name

---
+++ VapeShop.Service/Interfaces/ILiquidService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Entity.Product;
+using VapeShop.Domain.Response;
+
+namespace VapeShop.Service.Interfaces
+{
+	public interface ILiquidService
+	{
+		Task<BaseResponse<Liquid>> Get(int id);
+
+		Task<BaseResponse<IEnumerable<Liquid>>> GetAll();
+
+		Task<BaseResponse<Liquid>> Create(Liquid model);
+
+		Task<BaseResponse<Liquid>> Update(Liquid model);
+
+		Task<BaseResponse<bool>> Delete(Liquid model);
+
+	}
+}
