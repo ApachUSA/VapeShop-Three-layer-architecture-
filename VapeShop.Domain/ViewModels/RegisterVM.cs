4ec36c05d07304e95e4d945aa78232210a2499b7
ApachUA <4971779@gmail.com>
2023-08-02 16:15:34 +0300

    Created User ViewModels

---
+++ VapeShop.Domain/ViewModels/RegisterVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VapeShop.Domain.ViewModels
+{
+	public class RegisterVM
+	{
+		public string Surname { get; set; }
+
+		public string Name { get; set; }
+
+		public string? Phone { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+
+		public string PasswordConfirme { get; set; }
+	}
+}
