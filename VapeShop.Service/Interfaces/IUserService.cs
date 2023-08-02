9747fafc95907d08f9c5a909f31cbce80c017ea5
ApachUA <4971779@gmail.com>
2023-08-02 16:16:35 +0300

    Created interfaces for user services

---
+++ VapeShop.Service/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Response;
+using VapeShop.Domain.ViewModels;
+
+namespace VapeShop.Service.Interfaces
+{
+	public interface IUserService
+	{
+		Task<BaseResponse<IEnumerable<UserViewModel>>> GetAll();
+
+		Task<BaseResponse<UserViewModel>> Get(int id);
+
+		Task<BaseResponse<bool>> Delete(int id);
+	}
+}
