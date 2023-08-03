b14804e772d10dbb915626742caf29df2b5cd819
ApachUA <4971779@gmail.com>
2023-08-03 14:46:09 +0300

    Added communication method during register

--- VapeShop.Domain/ViewModels/RegisterVM.cs
+++ VapeShop.Domain/ViewModels/RegisterVM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VapeShop.Domain.Entity.Client;
 
 namespace VapeShop.Domain.ViewModels
 {
@@ -12,12 +13,14 @@ namespace VapeShop.Domain.ViewModels
 
 		public string Name { get; set; }
 
-		public string? Phone { get; set; }
+		public string Phone { get; set; }
 
 		public string Email { get; set; }
 
 		public string Password { get; set; }
 
 		public string PasswordConfirme { get; set; }
+
+		public int CommunicationMethodID { get; set; } = 1;
 	}
 }
