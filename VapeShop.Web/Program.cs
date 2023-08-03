1422d48b92394692b40d38594c98c55a6d2c02fd
ApachUA <4971779@gmail.com>
2023-08-03 14:49:51 +0300

    Registered  IAccountService and CookieAuthenticationDefaults

--- VapeShop.Web/Program.cs
+++ VapeShop.Web/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using VapeShop.Infrastructure;
 using VapeShop.Infrastructure.Interfaces;
 using VapeShop.Infrastructure.Repositories;
+using VapeShop.Service.Implementations;
+using VapeShop.Service.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,15 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<VapeShopDbContext>(options =>
 						options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped(typeof(IBaseRespository<>), typeof(Respository<>));
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IAccountService, AccountService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie(options =>
+	{
+		options.LoginPath = new PathString("/Account/Login");
+		options.AccessDeniedPath = new PathString("/Account/Login");
+	});
 
 var app = builder.Build();
 
