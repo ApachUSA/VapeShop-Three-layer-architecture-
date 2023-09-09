8f74ec86a8306c405e738ef50af63d5134a1f6b7
ApachUA <4971779@gmail.com>
2023-09-09 03:03:48 +0300

    Implementated ILiquidParamService

---
+++ VapeShop.Service/Implementations/LiquidParamService.cs
@@ -0,0 +1,292 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Entity.Product;
+using VapeShop.Domain.Enum;
+using VapeShop.Domain.Response;
+using VapeShop.Infrastructure.Interfaces;
+using VapeShop.Service.Interfaces;
+
+namespace VapeShop.Service.Implementations
+{
+	public class LiquidParamService : ILiquidParamService
+	{
+		private readonly IBaseRepository<Liquid_param> _liquid_paramRepository;
+		private readonly IBaseRepository<Nicotine> _nicotineRepository;
+		private readonly IBaseRepository<PG_VG> _pg_vgRepository;
+		private readonly IBaseRepository<Flavor> _flavorRepository;
+
+		public LiquidParamService(IBaseRepository<Liquid_param> liquid_paramRepository, IBaseRepository<Nicotine> nicotineRepository, IBaseRepository<PG_VG> pg_vgRepository, IBaseRepository<Flavor> flavorRepository)
+		{
+			_liquid_paramRepository = liquid_paramRepository;
+			_nicotineRepository = nicotineRepository;
+			_pg_vgRepository = pg_vgRepository;
+			_flavorRepository = flavorRepository;
+		}
+
+		public async Task<BaseResponse<bool>> Create(Liquid_param model)
+		{
+			try
+			{
+				await _liquid_paramRepository.Create(model);
+				return new BaseResponse<bool>()
+				{
+					Value = true,
+					Descrition = "Liquid_param added",
+					StatusCode = StatusCode.Succes
+
+				};
+			}
+			catch (Exception ex)
+			{
+				return new BaseResponse<bool>()
+				{
+					Descrition = $"[CreateLiquid_param] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+		}
+
+		public async Task<BaseResponse<bool>> Create(List<Liquid_param> model)
+		{
+			try
+			{
+				foreach (var param in model) await _liquid_paramRepository.Create(param);
+				return new BaseResponse<bool>()
+				{
+					Value = true,
+					Descrition = "Liquid_params added",
+					StatusCode = StatusCode.Succes
+
+				};
+			}
+			catch (Exception ex)
+			{
+				return new BaseResponse<bool>()
+				{
+					Descrition = $"[CreateLiquid_params] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+		}
+
+		public async Task<BaseResponse<bool>> Delete(Liquid_param model)
+		{
+			try
+			{
+
+				await _liquid_paramRepository.Delete(model);
+				return new BaseResponse<bool>()
+				{
+					Value = true,
+					StatusCode = StatusCode.Succes
+
+				};
+
+			}
+			catch (Exception ex)
+			{
+				return new BaseResponse<bool>()
+				{
+					Descrition = $"[DeleteLiquidParam] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+		}
+
+		public async Task<BaseResponse<bool>> Delete(int liquidParamID)
+		{
+			try
+			{
+				var liquid_param = await _liquid_paramRepository.Get().FirstOrDefaultAsync(x => x.LiquidParamID == liquidParamID);
+				if (liquid_param != null)
+				{
+					await _liquid_paramRepository.Delete(liquid_param);
+					return new BaseResponse<bool>()
+					{
+						Value = true,
+						StatusCode = StatusCode.Succes
+
+					};
+				}
+				return new BaseResponse<bool>()
+				{
+					Value = false,
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+			catch (Exception ex)
+			{
+				return new BaseResponse<bool>()
+				{
+					Descrition = $"[DeleteLiquidParam] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+		}
+
+		public async Task<BaseResponse<Liquid_param>> Get(int id)
+		{
+			try
+			{
+				var liquid = await _liquid_paramRepository.Get().FirstOrDefaultAsync(x => x.LiquidParamID == id);
+
+				if (liquid == null)
+				{
+					return new BaseResponse<Liquid_param>()
+					{
+						Descrition = "Liquid_param not found",
+						StatusCode = StatusCode.InternalServerError
+
+					};
+				}
+
+				return new BaseResponse<Liquid_param>()
+				{
+					Value = liquid,
+					StatusCode = StatusCode.Succes
+				};
+			}
+			catch (Exception ex)
+			{
+
+				return new BaseResponse<Liquid_param>()
+				{
+					Descrition = $"[GetLiquid_param] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+		}
+
+		public Task<BaseResponse<IEnumerable<Liquid_param>>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public BaseResponse<IEnumerable<Flavor>> GetFlavors()
+		{
+			try
+			{
+				var flavors = _flavorRepository.Get().ToList();
+
+				if (flavors == null)
+				{
+					return new BaseResponse<IEnumerable<Flavor>>()
+					{
+						Descrition = "Flavors not found",
+						StatusCode = StatusCode.InternalServerError
+
+					};
+				}
+
+				return new BaseResponse<IEnumerable<Flavor>>()
+				{
+					Value = flavors,
+					StatusCode = StatusCode.Succes
+				};
+			}
+			catch (Exception ex)
+			{
+
+				return new BaseResponse<IEnumerable<Flavor>>()
+				{
+					Descrition = $"[GetFlavors] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+
+				};
+			}
+		}
+		public BaseResponse<IEnumerable<Nicotine>> GetNicotine()
+		{
+			try
+			{
+				var nicotines = _nicotineRepository.Get().ToList();
+
+				if (nicotines == null)
+				{
+					return new BaseResponse<IEnumerable<Nicotine>>()
+					{
+						Descrition = "Nicotines not found",
+						StatusCode = StatusCode.InternalServerError
+
+					};
+				}
+
+				return new BaseResponse<IEnumerable<Nicotine>>()
+				{
+					Value = nicotines,
+					StatusCode = StatusCode.Succes
+				};
+			}
+			catch (Exception ex)
+			{
+
+				return new BaseResponse<IEnumerable<Nicotine>>()
+				{
+					Descrition = $"[GetNicotine] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+		}
+
+		public Task<BaseResponse<IEnumerable<Nicotine>>> GetNicotine(int liquid_id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public BaseResponse<IEnumerable<PG_VG>> GetPG_VG()
+		{
+			try
+			{
+				var pG_VGs = _pg_vgRepository.Get().ToList();
+
+				if (pG_VGs == null)
+				{
+					return new BaseResponse<IEnumerable<PG_VG>>()
+					{
+						Descrition = "Nicotines not found",
+						StatusCode = StatusCode.InternalServerError
+
+					};
+				}
+
+				return new BaseResponse<IEnumerable<PG_VG>>()
+				{
+					Value = pG_VGs,
+					StatusCode = StatusCode.Succes
+				};
+			}
+			catch (Exception ex)
+			{
+
+				return new BaseResponse<IEnumerable<PG_VG>>()
+				{
+					Descrition = $"[GetPG_VG] : {ex.Message}",
+					StatusCode = StatusCode.InternalServerError
+
+				};
+			}
+		}
+
+		public Task<BaseResponse<IEnumerable<PG_VG>>> GetPG_VG(int liquid_id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<BaseResponse<Liquid_param>> Update(Liquid_param model)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
