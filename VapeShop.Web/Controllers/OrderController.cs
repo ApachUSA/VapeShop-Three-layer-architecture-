df97a8669c951dfaf3b74ae249aa663ff4059d38
ApachUA <4971779@gmail.com>
2023-09-11 19:26:59 +0300

    Test method for start work on order

---
+++ VapeShop.Web/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VapeShop.Web.Controllers
+{
+	public class OrderController : Controller
+	{
+		[HttpPost]
+		public IActionResult Index(int liquid_paramID, int count)
+		{
+			return View();
+		}
+	}
+}
