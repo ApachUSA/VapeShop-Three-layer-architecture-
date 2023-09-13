0ba5bb1d696bace82f0db74cd59bf0c4ea344311
ApachUA <4971779@gmail.com>
2023-09-13 19:55:42 +0300

    Beginning development of the comparison function

---
+++ VapeShop.Web/Controllers/ComparsionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VapeShop.Web.Controllers
+{
+	public class ComparsionController : Controller
+	{
+		public IActionResult ComparsionIndex()
+		{
+			return View();
+		}
+	}
+}
