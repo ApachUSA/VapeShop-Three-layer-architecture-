97abc6b05ded6592ce8b8efa76b955bc53e79058
ApachUA <4971779@gmail.com>
2023-09-13 12:17:55 +0300

    Created IWishService interface

---
+++ VapeShop.Service/Interfaces/IWishService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VapeShop.Domain.Entity.Functions;
+using VapeShop.Domain.Entity.Product;
+using VapeShop.Domain.Response;
+
+namespace VapeShop.Service.Interfaces
+{
+	public interface IWishService
+	{
+		Task<BaseResponse<WishList>> Get(int userID);
+		Task<BaseResponse<IEnumerable<WishList>>> GetAll(int userID);
+
+		Task<BaseResponse<bool>> Create(WishList model);
+
+		Task<BaseResponse<bool>> Delete(int wishListID);
+		Task<BaseResponse<bool>> DeleteAll(int userID);
+	}
+}
